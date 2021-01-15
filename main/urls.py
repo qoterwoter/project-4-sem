233a3eeb73f3a834a29d7eca805121d107bc2279
qoterwater <marderics@gmail.com>
2021-01-15 21:02:25 +0300

    docs\ git commit -m docs

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.index, name='main'),
     path('about', views.about, name='about'),
     path('landing', views.landing, name='landing'),
-    path('admin', views.admin, name='admin'),
     path('students/', views.StudentsView.as_view(), name='students'),
     path('projects/', views.ProjectsView.as_view(), name='projects'),
 ]
